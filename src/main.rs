9f6f87cc94c87f4d38dba129db1cae4d5e8cefc2
tomas <tomasrolb@gmail.com>
2025-03-13 22:38:02 -0300

    Implemented path parameter extraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{BufRead, BufReader, Read};
 use std::net::TcpListener;
 
 fn main() {
@@ -5,7 +6,28 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Tuki");
+
+        let mut buf_reader = BufReader::new(stream);
+        let optional_line = buf_reader.lines()
+            .find(|result| result.as_ref().unwrap().starts_with("GET /api/"));
+
+        match optional_line {
+            None => { println!("Invalid request"); }
+            Some(Err(_)) => { println!("Invalid request"); }
+            Some(Ok(line)) => {
+                parse_path(line);
+            }
+        }
+    }
+}
+
+fn parse_path(line_posta: String) {
+    let path = line_posta.split(" ").collect::<Vec<&str>>()[1];
+    let num_as_string = path.split("/").collect::<Vec<&str>>()[2];
+
+    match str::parse::<u16>(num_as_string) {
+        Err(err_message) => println!("{}", err_message),
+        Ok(parsed_num) => println!("{}", parsed_num)
     }
 }
 
