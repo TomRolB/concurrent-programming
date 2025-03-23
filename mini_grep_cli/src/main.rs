e17c8525585185f04ba96918d989a1f51f950d7e
tomas <tomasrolb@gmail.com>
2025-03-23 12:51:10 -0300

    Fixed compilation errors and formatted main

--- mini_grep_cli/src/main.rs
+++ mini_grep_cli/src/main.rs
@@ -1,26 +1,37 @@
+use crate::CliErr::{FileNotFound, MissingFiles, MissingMode, MissingPattern, UnknownMode};
+use mini_grep::{grep_chunk, grep_conc, grep_seq};
 use std::env;
 use std::fs::File;
-use mini_grep::grep_seq;
-use crate::CliErr::{FileNotFound, MissingFiles, MissingMode, MissingPattern, UnknownMode};
 
 enum CliErr {
     MissingMode,
     MissingPattern,
     MissingFiles,
     FileNotFound(String),
-    UnknownMode(String)
+    UnknownMode(String),
 }
 
 fn main() {
     // TODO: Better error messages. Should display the desired command structure,
     //  explaining each argument
     match run() {
-        Ok(()) => {},
-        MissingMode => println!("No mode was passed. Must be one of 'seq', 'conc' and 'c-chunk.'"),
-        MissingPattern => println!("No pattern was passed. Must be a string to be searched."),
-        MissingFiles => println!("No file names were passed. Must be at least one."),
-        FileNotFound(file_nane) => println!("Could not find file '{}'.", file_nane),
+        Ok(()) => {}
+        Err(MissingMode) => {
+            print_error("No mode was passed. Must be one of 'seq', 'conc' and 'c-chunk.'")
+        }
+        Err(MissingPattern) => {
+            print_error("No pattern was passed. Must be a string to be searched.")
+        }
+        Err(MissingFiles) => print_error("No file names were passed. Must be at least one."),
+        Err(FileNotFound(file_name)) => {
+            print_error(format!("Could not find file '{}'.", file_name).as_str())
+        }
+        Err(UnknownMode(mode)) => print_error(format!("Unknown mode '{}'.", mode).as_str()),
     };
+}
+
+fn print_error(message: &str) {
+    println!("{}Error{}: {}", "\x1B[31m", "\x1B[0m", message)
 }
 
 fn run() -> Result<(), CliErr> {
@@ -28,35 +39,40 @@ fn run() -> Result<(), CliErr> {
 
     let mode = args.get(1).ok_or(MissingMode)?;
     let pattern = args.get(2).ok_or(MissingPattern)?;
-    let file_names = get_remaining(args)?;
+    let file_names = get_remaining(&args)?;
 
     let files: Vec<File> = open_files(file_names)?;
 
     match mode.as_str() {
         "seq" => print_all(grep_seq(pattern.clone(), files)),
-        "conc" => print_all(grep_seq(pattern.clone(), files)),
-        "c-chunk" => print_all(grep_seq(pattern.clone(), files)),
-        _ => Err(UnknownMode(mode.clone()))
+        "conc" => print_all(grep_conc(&pattern, files)),
+        "c-chunk" => print_all(grep_chunk(&pattern, files, 10000)),
+        // TODO: chunk size should actually be passed.
+        //  If the command is of the form:
+        //  cargo run -- c-chunk 5 tell frankenstein.txt romeo_and_juliet.txt
+        //  then the code above should actually be updated to iterate args,
+        //  instead of accessing positions directly
+        _ => Err(UnknownMode(mode.clone())),
     }
 }
 
-fn get_remaining<'a>(args: Vec<String>) -> Result<&'a [String], MissingFiles> {
+fn get_remaining(args: &Vec<String>) -> Result<&[String], CliErr> {
     let file_names = &args[3..];
 
-    if file_names.is_empty() { return Err(MissingFiles) };
+    if file_names.is_empty() {
+        return Err(MissingFiles);
+    };
     Ok(file_names)
 }
 
-fn open_files(file_names: &[String]) -> Result<Vec<File>, FileNotFound> {
-    file_names.iter()
-        .map(|file_name| {
-            File::open(file_name)
-                .map_err(|err| FileNotFound(file_name.clone()))
-        })
+fn open_files(file_names: &[String]) -> Result<Vec<File>, CliErr> {
+    file_names
+        .iter()
+        .map(|file_name| File::open(file_name).map_err(|_| FileNotFound(file_name.clone())))
         .collect()
 }
 
-fn print_all(filtered_lines: Vec<String>) -> Ok(()) {
+fn print_all(filtered_lines: Vec<String>) -> Result<(), CliErr> {
     filtered_lines.iter().for_each(|line| println!("{}", line));
     Ok(())
 }
