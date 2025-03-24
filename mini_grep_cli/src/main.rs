d49c62235d2bffc8f50d09a244421f1be65c0318
tomas <tomasrolb@gmail.com>
2025-03-24 12:14:12 -0300

    Chunk size is now retrieved. Arguments are now obtained from an iterator.

--- mini_grep_cli/src/main.rs
+++ mini_grep_cli/src/main.rs
@@ -1,10 +1,14 @@
-use crate::CliErr::{FileNotFound, MissingFiles, MissingMode, MissingPattern, UnknownMode};
+use crate::CliErr::{FileNotFound, MissingFiles, MissingChunkSize, ParseChunkSizeError, MissingMode, MissingPattern, UnknownMode};
 use mini_grep::{grep_chunk, grep_conc, grep_seq};
 use std::env;
+use std::env::Args;
 use std::fs::File;
+use std::str::FromStr;
 
 enum CliErr {
     MissingMode,
+    MissingChunkSize,
+    ParseChunkSizeError,
     MissingPattern,
     MissingFiles,
     FileNotFound(String),
@@ -27,6 +31,12 @@ fn main() {
             print_error(format!("Could not find file '{}'.", file_name).as_str())
         }
         Err(UnknownMode(mode)) => print_error(format!("Unknown mode '{}'.", mode).as_str()),
+        Err(MissingChunkSize) => {
+            print_error("No chunk size was passed, even though selected option is 'c-chunk'".as_ref())
+        },
+        Err(ParseChunkSizeError) => {
+            print_error("Could not parse chunk size".as_ref())
+        },
     };
 }
 
@@ -35,40 +45,46 @@ fn print_error(message: &str) {
 }
 
 fn run() -> Result<(), CliErr> {
-    let args: Vec<String> = env::args().collect();
+    let mut args = env::args();
+    // First one is the command name and is unused
+    args.next();
 
-    let mode = args.get(1).ok_or(MissingMode)?;
-    let pattern = args.get(2).ok_or(MissingPattern)?;
-    let file_names = get_remaining(&args)?;
+    let mode = args.next().ok_or(MissingMode)?;
+    let chunk_size = get_chunk_size(&mut args, &mode)?;
+
+    let pattern = args.next().ok_or(MissingPattern)?;
+    let file_names = get_remaining(&mut args)?;
 
     let files: Vec<File> = open_files(file_names)?;
 
     match mode.as_str() {
         "seq" => print_all(grep_seq(pattern.clone(), files)),
         "conc" => print_all(grep_conc(&pattern, files)),
-        "c-chunk" => print_all(grep_chunk(&pattern, files, 10000)),
-        // TODO: chunk size should actually be passed.
-        //  If the command is of the form:
-        //  cargo run -- c-chunk 5 tell frankenstein.txt romeo_and_juliet.txt
-        //  then the code above should actually be updated to iterate args,
-        //  instead of accessing positions directly
+        "c-chunk" => print_all(grep_chunk(&pattern, files, chunk_size)),
         _ => Err(UnknownMode(mode.clone())),
     }
 }
 
-fn get_remaining(args: &Vec<String>) -> Result<&[String], CliErr> {
-    let file_names = &args[3..];
-
-    if file_names.is_empty() {
-        return Err(MissingFiles);
-    };
-    Ok(file_names)
+fn get_chunk_size(args: &mut Args, mode: &String) -> Result<usize, CliErr> {
+    if mode == "c-chunk" {
+        let chunks_as_str = args.next().ok_or(MissingChunkSize)?;
+        usize::from_str(chunks_as_str.as_str())
+            .map_err(|_| ParseChunkSizeError)
+    } else {
+        Ok(0)
+    }
 }
 
-fn open_files(file_names: &[String]) -> Result<Vec<File>, CliErr> {
+fn get_remaining(args: &mut Args) -> Result<impl Iterator<Item = String>, CliErr> {
+    if let None = args.peekable().peek() {
+        return Err(MissingFiles);
+    };
+    Ok(args)
+}
+
+fn open_files(file_names: impl Iterator<Item = String>) -> Result<Vec<File>, CliErr> {
     file_names
-        .iter()
-        .map(|file_name| File::open(file_name).map_err(|_| FileNotFound(file_name.clone())))
+        .map(|file_name| File::open(&file_name).map_err(|_| FileNotFound(file_name.clone())))
         .collect()
 }
 
